9b344a46fa3db4084d7a5a5a8c4c7c3b0f370dc9
Алексей Шкарупа <shkarupa.alex@gmail.com>
2017-09-05 09:14:09 +0300

    Добавлена обработка ошибок при компиляции html

--- test/levels/blocks.common/add-template/add-template.bh.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(bh) {
-  bh.match('add-template', function (ctx) {
-    ctx.attr('data-test', '123');
-    return {
-      block: 'add-template-wrapper',
-      content: ctx.json()
-    };
-  });
-};
